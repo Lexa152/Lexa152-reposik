e6858c4c9d33110c58399807d9635c9be94a0d9a
Лёха К <Lexin@email.com>
2026-06-01 01:28:25 +0300

    маски для номеров

---
+++ src/masks.py
@@ -0,0 +1,12 @@
+def get_mask_card_number(num_k: str) -> str:
+    """Функция для наложения маски на номер банковой карты"""
+    if len(num_k) == 16:
+        num_km = num_k[0:6] + "******" + num_k[12:16]
+    return num_km[0:4] + " " + num_km[4:8] + " " + num_km[8:12] + " " + num_km[12:16]
+
+
+def get_mask_account(num_s: str) -> str:
+    """Функция для наложения маски на номер счета"""
+    if len(num_s) >= 4:
+        num_sm = "**" + num_s[-4:]
+    return num_sm
