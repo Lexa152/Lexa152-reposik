65f65576558bcfd5ec1e9de7c202534993a848a8
Лёха К <Lexin@email.com>
2026-06-01 23:53:40 +0300

    work on bugs

--- src/widget.py
+++ src/widget.py
@@ -9,6 +9,8 @@ def mask_account_card(str_in: str) -> str:
             return str_in[0:(len(str_in)-17)]+' '+get_mask_card_number(str_in[(len(str_in)-16):])
         if str_in[0:4] == 'Счет': #СЧЁТ
             return 'Счет '+get_mask_account(str_in[5:])
+    else:
+        return 'нет данных'
 
 
 def get_date(date_string):
