8d28f6999c73ef74b47480834eb20e3430811b11
Лёха К <Lexin@email.com>
2026-06-01 19:11:41 +0300

    data and mask

--- src/widget.py
+++ src/widget.py
@@ -8,16 +8,6 @@ def mask_account_card(str_in: str) -> str:
         if str_in[0:4] == 'Счет': #СЧЁТ
             return 'Счет '+get_mask_account(str_in[5:])
 
-
-#str0 = ''
-#str0 = 'Maestro 1596837868705199'
-#str0 = 'Счет 64686473678894779589'
-str0 = 'MasterCard 7158300734726758'
-#str0 = 'Счет 35383033474447895560'
-#str0 = 'Visa Classic 6831982476737658'
-#str0 = 'Visa Platinum 8990922113665229'
-#str0 = 'Visa Gold 5999414228426353'
-#str0 = 'Счет 73654108430135874305'
-
-print(mask_account_card(str0))
-#num_sm = "**" + num_s[-4:]
+def get_date(dtin: str) -> str:
+    '''Конверт времени'''
+    return dtin[8:10]+'.'+dtin[5:7]+'.'+dtin[0:4]
