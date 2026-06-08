b82b823743502fa16ced3685a684fee835dfd974
Лёха К <Lexin@email.com>
2026-06-08 21:24:08 +0300

    add sort dicts

--- src/processing.py
+++ src/processing.py
@@ -6,5 +6,7 @@ def filter_by_state(data: List[Dict[str, any]], state: str = 'EXECUTED') -> List
     return [item for item in data if item.get("state") == state]
 
 
-
+def sort_by_date(data: List[Dict[str, any]], reverse: bool = True) -> List[Dict[str, any]]:
+    """ Сортирует список словарей по значению ключа """
+    return sorted(data, key=lambda item: item['date'], reverse=reverse)
 
