1e182632c73a0b019b6e86691f25325ecb29f32f
Лёха К <Lexin@email.com>
2026-06-08 21:41:53 +0300

    add filter dicts

--- src/processing.py
+++ src/processing.py
@@ -4,9 +4,3 @@ from typing import List, Dict
 def filter_by_state(data: List[Dict[str, any]], state: str = 'EXECUTED') -> List[Dict[str, any]]:
     """ Фильтрует список словарей по значению ключа """
     return [item for item in data if item.get("state") == state]
-
-
-def sort_by_date(data: List[Dict[str, any]], reverse: bool = True) -> List[Dict[str, any]]:
-    """ Сортирует список словарей по значению ключа """
-    return sorted(data, key=lambda item: item['date'], reverse=reverse)
-
